079b72d44774604b8dd3140cf76bd962f46c3db9
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-06-23 05:19:45 +0200

    Added install.php

---
+++ install.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * libBitly
+ *
+ * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @link http://phpmanufaktur.de
+ * @copyright 2012
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {
+  if (defined('LEPTON_VERSION'))
+    include(WB_PATH.'/framework/class.secure.php');
+}
+else {
+  $oneback = "../";
+  $root = $oneback;
+  $level = 1;
+  while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+    $root .= $oneback;
+    $level += 1;
+  }
+  if (file_exists($root.'/framework/class.secure.php')) {
+    include($root.'/framework/class.secure.php');
+  }
+  else {
+    trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+  }
+}
+// end include class.secure.php
+
+require_once WB_PATH.'/modules/lib_bitly/library.php';
+
+global $admin;
+
+$cfg = new bitlyConfig();
+if (!$cfg->createTable()) {
+  // Prompt Errors
+  $admin->print_error($cfg->getError());
+}
